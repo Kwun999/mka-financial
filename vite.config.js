681ad56508b23dae86ff9f078b6cfe41f0cc4cab
Kwun999 <michelleandkwun@gmail.com>
2026-04-10 16:06:01 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
-node_modules
-dist
-.DS_Store
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/mka-financial/',
+})
